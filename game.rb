6f778729da7e9cb5ee5a1e7a2f50fb0dfeaa8300
Daniel Frydman <danielfrydman@id.uff.br>
2022-12-22 23:47:17 -0300

    beginning of architecture implementation

--- game.rb
+++ game.rb
@@ -1,104 +1,180 @@
 class Game
   def initialize
-    @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    @com = "X" # the computer's marker
-    @hum = "O" # the user's marker
+    @type_of_game = TypeOfGame.new
+    @difficulty = Difficulty.new
+    @board = Board.new
   end
-
-  def start_game
-    # start by printing the board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-    puts "Enter [0-8]:"
-    # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
-      get_human_spot
-      if !game_is_over(@board) && !tie(@board)
-        eval_board
-      end
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
-    end
-    puts "Game over"
-  end
-
-  def get_human_spot
-    spot = nil
-    until spot
-      spot = gets.chomp.to_i
-      if @board[spot] != "X" && @board[spot] != "O"
-        @board[spot] = @hum
-      else
-        spot = nil
-      end
-    end
-  end
-
-  def eval_board
-    spot = nil
-    until spot
-      if @board[4] == "4"
-        spot = 4
-        @board[spot] = @com
-      else
-        spot = get_best_move(@board, @com)
-        if @board[spot] != "X" && @board[spot] != "O"
-          @board[spot] = @com
-        else
-          spot = nil
-        end
-      end
-    end
-  end
-
-  def get_best_move(board, next_player, depth = 0, best_score = {})
-    available_spaces = []
-    best_move = nil
-    board.each do |s|
-      if s != "X" && s != "O"
-        available_spaces << s
-      end
-    end
-    available_spaces.each do |as|
-      board[as.to_i] = @com
-      if game_is_over(board)
-        best_move = as.to_i
-        board[as.to_i] = as
-        return best_move
-      else
-        board[as.to_i] = @hum
-        if game_is_over(board)
-          best_move = as.to_i
-          board[as.to_i] = as
-          return best_move
-        else
-          board[as.to_i] = as
-        end
-      end
-    end
-    if best_move
-      return best_move
-    else
-      n = rand(0..available_spaces.count)
-      return available_spaces[n].to_i
-    end
-  end
-
-  def game_is_over(b)
-
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
-  end
-
-  def tie(b)
-    b.all? { |s| s == "X" || s == "O" }
-  end
-
 end
 
-game = Game.new
-game.start_game
+# class Game
+#   def initialize
+#     @board = %w[0 1 2 3 4 5 6 7 8]
+#     @computer = 'X'
+#     @user = 'O'
+#     @difficulty = 1
+#   end
+
+#   def start_game
+#     choose_difficulty
+
+#     choose_type_of_game
+
+#     initialize_message
+#     print_board
+#     puts 'Enter [0-8]:'
+
+#     until no_more_moves?
+#       get_user_spot
+#       evaluate_board unless no_more_moves?
+#       print_board
+#     end
+
+#     puts 'Game over'
+#   end
+
+#   private
+
+#   def choose_difficulty
+#     puts 'Choose the difficulty of your game:'
+#     puts "[1] - Easy\n[2] - Medium\n[3] - Hard"
+#     difficulty = gets.chomp.to_i
+#     until difficulty.is_a? Integer && (difficulty.positive? && difficulty < 4)
+#       puts 'Please, choose one of the following numbers:'
+#       puts "[1] - Easy\n[2] - Medium\n[3] - Hard"
+#     end
+
+#   end
+
+#   def choose_type_of_game
+#   end
+
+#   def initialize_message
+#     puts '================================================================'
+#     puts "Let's go:"
+#   end
+
+#   def get_user_spot(spot = nil)
+#     until spot
+#       spot = gets.chomp.to_i
+
+#       if space_not_occupied?(@board[spot])
+#         @board[spot] = @user
+#         next
+#       end
+
+#       puts 'Occupied space, choose another spot:'
+#       spot = nil
+#     end
+#   end
+
+#   def evaluate_board
+#     spot = nil
+#     until spot
+#       return assign_spot_to_computer(@board, 4) if middle_free?
+
+#       spot = get_best_move(@board)
+
+#       if space_not_occupied?(@board[spot])
+#         @board[spot] = @computer
+#         next
+#       end
+
+#       spot = nil
+#     end
+#   end
+
+#   def get_best_move(board)
+#     best_move = nil
+#     available_spaces = available_spaces(board)
+
+#     available_spaces.each do |available_space|
+#       available_space_integer = available_space.to_i
+#       assign_spot_to_computer(board, available_space_integer)
+
+#       if game_over?(board)
+#         best_move = available_space.to_i
+#         board[best_move] = available_space
+#         return best_move
+#       end
+
+#       board[available_space_integer] = @user
+
+#       if game_over?(board)
+#         best_move = available_space.to_i
+#         board[best_move] = available_space
+#         return best_move
+#       end
+
+#       board[available_space_integer] = available_space
+#     end
+
+#     return best_move unless best_move.nil?
+
+#     random_number = rand(0..available_spaces.count)
+#     available_spaces[random_number].to_i
+#   end
+
+#   def game_over?(board)
+#     board_options = [
+#       [board[0], board[1], board[2]],
+#       [board[3], board[4], board[5]],
+#       [board[6], board[7], board[8]],
+#       [board[0], board[3], board[6]],
+#       [board[1], board[4], board[7]],
+#       [board[2], board[5], board[8]],
+#       [board[0], board[4], board[8]],
+#       [board[2], board[4], board[6]]
+#     ]
+
+#     board_options.any? { |option| option.uniq.length == 1 }
+#   end
+
+#   def print_board
+#     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+#   end
+
+#   def tie?(board)
+#     board.all? { |space| space_occupied_by_user?(space) || space_occupied_by_computer?(space) }
+#   end
+
+#   def no_more_moves?
+#     game_over?(@board) || tie?(@board)
+#   end
+
+#   def middle_free?
+#     @board[4] == '4'
+#   end
+
+#   def assign_spot_to_computer(board, spot)
+#     board[spot] = @computer
+#   end
+
+#   def space_not_occupied?(space)
+#     space_not_occupied_by_user?(space) && space_not_occupied_by_computer?(space)
+#   end
+
+#   def space_occupied_by_user?(space)
+#     !space_not_occupied_by_user?(space)
+#   end
+
+#   def space_occupied_by_computer?(space)
+#     !space_not_occupied_by_computer?(space)
+#   end
+
+#   def space_not_occupied_by_user?(space)
+#     space != 'O'
+#   end
+
+#   def space_not_occupied_by_computer?(space)
+#     space != 'X'
+#   end
+
+#   def available_spaces(board)
+#     board.filter_map do |space|
+#       space if space_not_occupied?(space)
+#     end
+#   end
+# end
+
+# game = Game.new
+# game.start_game
